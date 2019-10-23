a9aa990001711393901da9091ff27c6c806d4988
Antosox <antoine.burgaudeau@epsi.fr>
2019-10-23 12:05:30 +0200

    jour 1

---
+++ README.txt
@@ -0,0 +1,5 @@
+jour-1:
+rien ne va plus... le survivant découvre une nouvelle terre mais elle est très éloignée.
+Il arrive finalement à terre et met en place ses premières ressources. Deux dépots et un projet commence !
+Il est cependant perdu sur cette nouvelle contrée qui s'étends devant lui...
+à suivre
---
+++ src/components/Login.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <v-form>
+      <v-text-field></v-text-field>
+    </v-form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'login',
+  props: {},
+  data() {
+    return {
+      msg: 'Login',
+      test: 'test'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
